2a1224da18f9aba2691a090515541a3826765410
JoaoGabrielVS19 <joaogabrielsilvaqdi@gmail.com>
2023-12-07 22:14:03 -0300

    docs: updated README

--- Drts.Lives.API/API/obj/Debug/net8.0/Drts.Lives.API.AssemblyInfo.cs
+++ Drts.Lives.API/API/obj/Debug/net8.0/Drts.Lives.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Drts.Lives.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ee18ec81e1b2ffce6a0633bd920cafbddbc23fd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Drts.Lives.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Drts.Lives.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
