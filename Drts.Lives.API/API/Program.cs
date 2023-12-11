cc13d5b901e61a29545ed89b662d95a7db790085
JoaoGabrielVS19 <joaogabrielsilvaqdi@gmail.com>
2023-12-10 23:54:51 -0300

    refactor: included swagger in production

--- Drts.Lives.API/API/Program.cs
+++ Drts.Lives.API/API/Program.cs
@@ -34,7 +34,7 @@ builder.Services.AddScoped<IEnrollmentRepository, EnrollmentRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
