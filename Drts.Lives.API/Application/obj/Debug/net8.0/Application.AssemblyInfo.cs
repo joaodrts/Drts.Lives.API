ac44c4191e4071c81561f7f501508bb8d0b122e7
JoaoGabrielVS19 <joaogabrielsilvaqdi@gmail.com>
2023-12-11 06:45:07 -0300

    refactor: updated appsetting

--- Drts.Lives.API/Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ Drts.Lives.API/Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5ad77af1c87d07f5c50934a0a274ac450339f35")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
